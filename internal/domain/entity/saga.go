9612969b5e6da3d4a5779222ab01aded18a9e370
Egor Kutov <kutov.egor@wildberries.work>
2025-06-19 13:50:16 +0300

    CHORE: lint checks

--- internal/domain/entity/saga.go
+++ internal/domain/entity/saga.go
@@ -13,15 +13,15 @@ const (
 )
 
 type Saga struct {
-	ID        int64
-	OrderID   int64
-	Status    SagaStatus
-	Steps     []Step
-	CreatedAt time.Time
-	UpdatedAt *time.Time
+	ID        int64      `json:"id"`
+	OrderID   int64      `json:"order_id"`
+	Status    SagaStatus `json:"status"`
+	Steps     []Step     `json:"steps"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty"`
 }
 
 type Step struct {
-	Name   string
-	Status string
+	Name   string `json:"name"`
+	Status string `json:"status"`
 }
